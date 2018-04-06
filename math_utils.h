763b8a8b273583a502d3096e42bea2b848fb8ca4
Amogh100 <2amogh@gmail.com>
2018-04-06 15:59:14 -0600

    Added method declarations to header

---
+++ math_utils.h
@@ -0,0 +1,9 @@
+#include <vector>
+
+class Math {
+    public:
+        static bool IsSquare(int num);
+        static int GetDigit(int num, int place);
+        static bool EqualParity(int x, int y);
+        static bool EqualParity(std::vector<int> nums);  
+};
